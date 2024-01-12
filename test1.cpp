1d90280d07cc0bcd996c7da283ecbb2710b56b09
Himanshuba9hel <himanshuba9hel@gmail.com>
2024-01-12 22:20:15 +0530

    add vector example

--- test1.cpp
+++ test1.cpp
@@ -1,2 +1,12 @@
-test 1
-fytuyioyoi
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //vector initialization
+    vector<int> v = {2,4,5,6,7};
+    for(int i = 0; i<v.size();i++){
+        cout<<"Vector ["<<i<<"] . "<<v[i]<<endl;
+    }
+    cout<<"hello"<<endl;
+    //cout<<v[0]<<""<<v[1]<<endl;
+    return 0;
+}
